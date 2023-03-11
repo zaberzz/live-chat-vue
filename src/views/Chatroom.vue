8290a531297039709d1c094d0af5f08897e59ff0
Saba <saba.sadegh67@gmail.com>
2023-03-11 16:13:54 +0330

    watch a user

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -6,8 +6,20 @@
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import getUser from "@/composable/getUser";
+import { watch } from "@vue/runtime-core";
+import { useRouter } from "vue-router";
 export default {
   components: { Navbar },
+  setup() {
+    const router = useRouter();
+    const { user } = getUser();
+    watch(user, () => {
+      if (!user.value) {
+        router.push({ name: "Welcome" });
+      }
+    });
+  },
 };
 </script>
 
